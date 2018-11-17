fd1a017bd7ed69734c791213a9f4a1fd272e1040
Christopher Humphrey <c.r.humphrey2@gmail.com>
2018-11-17 09:09:05 -0500

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -11,7 +11,7 @@ days_in_year = 365
 blocks = 2300000
 sec_in_year = sec_in_minute * minutes_in_hour * hours_in_day * days_in_year
 total_secs = sec_in_year * 20
-total_time = total_secs / blocks / 60
+total_time = blocks / total_secs / 60
 total_time = round(total_time)
 time_it_takes = "It takes {} minutes to cut & lay one block to build the Great Pyramid."
 print(time_it_takes.format(total_time))
