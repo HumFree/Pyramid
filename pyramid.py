a2afb16675f31ecef70b56d5f19f56616e84287a
Christopher Humphrey <c.r.humphrey2@gmail.com>
2018-11-24 15:36:17 -0500

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -4,13 +4,13 @@
 during the contruction of the Great Pyramid. """
 # It took 20 years to build with 2,300,000 blocks
 
-sec_in_minute = 60
+#sec_in_minute = 60
 minutes_in_hour = 60
 hours_in_day = 24
 days_in_year = 365.25
 blocks = 2300000
-sec_in_year = sec_in_minute * minutes_in_hour * hours_in_day * days_in_year
-total_secs = sec_in_year * 20
+min_in_year = (minutes_in_hour * hours_in_day * days_in_year
+total_secs = min_in_year * 20
 total_time = blocks / total_secs
 time_it_takes = "It takes {} seconds to cut & lay one block to build the Great Pyramid."
 print(time_it_takes.format(total_time))
